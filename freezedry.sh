38e063e5cd838a5226ede424cb4e2659477379a6
Justin Emsoff <justinemsoff@Justins-MacBook-Pro.local>
2024-01-20 22:08:27 -0800

    adding aws commands

--- freezedry.sh
+++ freezedry.sh
@@ -5,7 +5,7 @@ vault="$2"
 description="$3"
 chunkSize=268435456
 percentComplete=0;
-
+cumulativeHash
 echo "Chunking $file";
 
 fileCount=$(ls -1 | grep "^chunk" | wc -l)
@@ -14,20 +14,31 @@ echo "Total parts to upload: " $fileCount
 
 echo  "Initializing multipart upload for $file >> $vault with description $description";
 
+touch cumulativehash
+
+init=$(aws glacier initiate-multipart-upload --account-id - --part-size $chunkSize --vault-name $vault --archive-description $description)
+
+uploadId=$(echo $init | jq '.uploadId' | xargs)
+
 i=0;
-for file in $(ls | grep "^chunk") 
-  do
-    currentHash=$(sha256sum "$file" | awk '{print $1}')
-    cumulativeHash=$(echo -n "$cumulativeHash$currentHash" | sha256sum | awk '{print $1}')
+for file in $(ls | grep "^chunk")
+do
+    rangeStart=$((i*chunkSize))
+    rangeEnd=$((i*chunkSize+chunkSize-1))
+    
+	aws glacier upload-multipart-part --upload-id $uploadId --body $filef --range "'"'bytes '"$rangeStart"'-'"$rangeEnd"'/*'"'" --account-id - --vault-name $vault
+    
+	currentHash=$(openssl dgst -sha256 -binary "$file" | awk '{print $1}')
+    cumulativeHash=$(echo -n "$cumulativeHash$currentHash" | openssl dgst -sha256 -binary | awk '{print $1}')
+    
+    percentComplete=`echo $percentComplete+$percentIncrement|bc -l`
+    printf "%3.2f%% complete, now uploading $file\n" "$percentComplete"
+    
+    # sleep 1
+    i=$(($i+1))
+    echo "Cumulative hash after $file: $cumulativeHash"
+    echo $cumulativeHash > cumulativehash
+    
+done
 
-	byteStart=$((i*chunkSize))
-	byteEnd=$((i*chunkSize+chunkSize-1))
-
-	percentComplete=`echo $percentComplete+$percentIncrement|bc -l`
-	printf "%3.2f%% complete, now uploading $file\n" "$percentComplete"
-
-	sleep 1
-	i=$(($i+1))
-	echo "Cumulative hash after $file: $cumulativeHash"
-
-  done
+aws glacier complete-multipart-upload --checksum $cumulativeHash --archive-size 3145728 --upload-id $uploadId --account-id - --vault-name $vault
